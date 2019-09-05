9e071ef3d72f156bd2d9b18535bde6fd7d9e61a1
Blythe Pollard <blytheepollard@gmail.com>
2019-09-05 18:23:17 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,9 @@
 # Write a speak_to_grandma method.
 
-def speak_to_grandma
+def speak_to_grandma(phrase)
+  if phrase.capitalize
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif   
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
