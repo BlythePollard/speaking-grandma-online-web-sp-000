56f3e6ee060ca4f4d0e9c9d38fc7b9bda5579d05
Blythe Pollard <blytheepollard@gmail.com>
2019-09-05 18:29:56 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase.capitalize
+  elsif phrase.capitalize
     return "NO, NOT SINCE 1938!"
   elsif phrase != phrase.capitalize
     puts "HUH? SPEAK UP, SONNY!"
